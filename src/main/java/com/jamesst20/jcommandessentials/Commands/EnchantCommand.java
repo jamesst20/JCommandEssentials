c8f986b6f6c646986b2004adbd9c03870ba5bb67
James St-Pierre <jamesst20@gmail.com>
2012-11-15 18:08:31 -0500

    Improved /enchant + adding keyword all

--- src/main/java/com/jamesst20/jcommandessentials/Commands/EnchantCommand.java
+++ src/main/java/com/jamesst20/jcommandessentials/Commands/EnchantCommand.java
@@ -22,38 +22,27 @@ public class EnchantCommand implements CommandExecutor {
         if (args.length == 0) {
             String enchantList = "";
             for (Enchantment enchant : Enchantment.values()) {
-                enchantList += formatEnchantment(enchant.getName()) + ", ";
+                enchantList += "&4" + formatEnchantment(enchant.getName()) + "&3, ";
             }
-            Methods.replaceLast(enchantList, ", ", "");
+            enchantList = Methods.replaceLast(enchantList, ", ", "");
             Methods.sendPlayerMessage(cs, "/enchant <enchantment> <level>");
-            Methods.sendPlayerMessage(cs, "You can apply those enchantments : " + Methods.red(enchantList) + ".");
+            Methods.sendPlayerMessage(cs, Methods.coloring("You can apply those enchantments : " + "&4All&3, " + enchantList + "&3."));
             return true;
-        } else if (args.length == 2) {
-            try {
-                Player player = ((Player) cs);
-                Enchantment ench = Enchantment.getByName(args[0].toUpperCase());
-                int enchantLevel = Integer.parseInt(args[1]);
-                if (ench != null) {
-                    if (ench.canEnchantItem(player.getItemInHand())) {
-                        if (ench.getMaxLevel() >= enchantLevel && ench.getStartLevel() <= enchantLevel) {
-                            player.getItemInHand().addEnchantment(ench, enchantLevel);
-                            Methods.sendPlayerMessage(cs, "You applied " + Methods.red(formatEnchantment(ench.getName())) + " to the item you're holding.");
-                        }else{
-                            Methods.sendPlayerMessage(cs, "Minimum for this enchantment is " + Methods.red(String.valueOf(ench.getStartLevel())) + " and maximum is " + Methods.red(String.valueOf(ench.getMaxLevel()) + "."));
-                        }
-                        return true;
-                    } else {
-                        Methods.sendPlayerMessage(cs, "This enchantment can't be applied to this item.");
-                        return true;
-                    }
-                } else {
-                    Methods.sendPlayerMessage(cs, ChatColor.RED + "Enchantment not found.");
-                    return true;
-                }
-            } catch (NumberFormatException e) {
-                Methods.sendPlayerMessage(cs, ChatColor.RED + "Failed to get enchantment level. Did you give a number?");
+        } else if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("all")){
+                enchantAll(cs, ((Player) cs));
                 return true;
+            }else{
+                return false;
             }
+        } else if (args.length == 2) {
+            if (args[0].equalsIgnoreCase("all")) {
+                Methods.sendPlayerMessage(cs, Methods.red("Maximum level is used with keyword all."));
+                enchantAll(cs, ((Player) cs));
+            } else {
+                enchant(cs, ((Player) cs), args);
+            }
+            return true;
         } else {
             return false;
         }
@@ -70,5 +59,40 @@ public class EnchantCommand implements CommandExecutor {
         } else {
             return Character.toUpperCase(str.charAt(0)) + str.substring(1).toLowerCase();
         }
+    }
+
+    private void enchant(CommandSender cs, Player player, String[] args) {
+        try {
+            Enchantment ench = Enchantment.getByName(args[0].toUpperCase());
+            int enchantLevel = Integer.parseInt(args[1]);
+            if (ench != null) {
+                if (ench.canEnchantItem(player.getItemInHand())) {
+                    if (ench.getMaxLevel() >= enchantLevel && ench.getStartLevel() <= enchantLevel) {
+                        player.getItemInHand().addEnchantment(ench, enchantLevel);
+                        Methods.sendPlayerMessage(cs, "You applied " + Methods.red(formatEnchantment(ench.getName())) + " to the item you're holding.");
+                    } else {
+                        Methods.sendPlayerMessage(cs, "Minimum for this enchantment is " + Methods.red(String.valueOf(ench.getStartLevel())) + " and maximum is " + Methods.red(String.valueOf(ench.getMaxLevel()) + "."));
+                    }
+                } else {
+                    Methods.sendPlayerMessage(cs, "This enchantment can't be applied to this item.");
+                }
+            } else {
+                Methods.sendPlayerMessage(cs, ChatColor.RED + "Enchantment not found.");
+            }
+        } catch (NumberFormatException e) {
+            Methods.sendPlayerMessage(cs, ChatColor.RED + "Failed to get enchantment level. Did you give a number?");
+        }
+    }
+
+    private void enchantAll(CommandSender cs, Player player) {
+        String enchantments = "";
+        for (Enchantment ench : Enchantment.values()) {
+            if (ench.canEnchantItem(player.getItemInHand())) {
+                player.getItemInHand().addEnchantment(ench, ench.getMaxLevel());
+                enchantments += "&4" + formatEnchantment(ench.getName()) + "&3, ";
+            }
+        }
+        enchantments = Methods.replaceLast(enchantments, ", ", "");
+        Methods.sendPlayerMessage(cs, "You applied " + Methods.coloring(enchantments) + ChatColor.DARK_AQUA + " to the item you're holding.");
     }
 }
