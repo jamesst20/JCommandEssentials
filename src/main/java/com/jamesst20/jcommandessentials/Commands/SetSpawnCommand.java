043a5f51a52adf9a2c1db6fdf15ee5048b843813
James St-Pierre <jamesst20@gmail.com>
2012-11-20 23:01:41 -0500

    Nothing

--- src/main/java/com/jamesst20/jcommandessentials/Commands/SetSpawnCommand.java
+++ src/main/java/com/jamesst20/jcommandessentials/Commands/SetSpawnCommand.java
@@ -15,36 +15,35 @@ import com.jamesst20.jcommandessentials.Utils.Methods;
 
 public class SetSpawnCommand implements CommandExecutor {
 
-	@Override
-	public boolean onCommand(CommandSender cs, Command command, String cmd, String[] args) {
-		if (!Methods.hasPermissionTell(cs, "JCMDEss.commands.setspawn")) {
-			return true;
-		}
-		if (Methods.isConsole(cs)) {
-			Methods.sendPlayerMessage(cs, ChatColor.RED + "The console can't use /setspawn.");
-			return true;
-		}
-		if (args.length == 0) {
-                    Location location = ((Player)cs).getLocation();
-			setDefaultSpawn(location);
-                        ((Player)cs).getWorld().setSpawnLocation((int)location.getX(), (int)location.getY(), (int)location.getZ());
-			Methods.sendPlayerMessage(cs, "Default spawn set to your location.");
-			return true;
-		} else {
-			return false;
-		}
-	}
+    @Override
+    public boolean onCommand(CommandSender cs, Command command, String cmd, String[] args) {
+        if (!Methods.hasPermissionTell(cs, "JCMDEss.commands.setspawn")) {
+            return true;
+        }
+        if (Methods.isConsole(cs)) {
+            Methods.sendPlayerMessage(cs, ChatColor.RED + "The console can't use /setspawn.");
+            return true;
+        }
+        if (args.length == 0) {
+            Location location = ((Player) cs).getLocation();
+            setDefaultSpawn(location);
+            ((Player) cs).getWorld().setSpawnLocation((int) location.getX(), (int) location.getY(), (int) location.getZ());
+            Methods.sendPlayerMessage(cs, "Default spawn set to your location.");
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	private void setDefaultSpawn(Location loc) {
-		File spawnConfigFile = Config.getConfigFile("spawns.yml");
-		YamlConfiguration spawnConfig = Config.getCustomConfig(spawnConfigFile);
-		spawnConfig.set("spawns.default.world", loc.getWorld().getName());
-		spawnConfig.set("spawns.default.x", loc.getX());
-		spawnConfig.set("spawns.default.y", loc.getY());
-		spawnConfig.set("spawns.default.z", loc.getZ());
-		spawnConfig.set("spawns.default.yaw", loc.getYaw());
-		spawnConfig.set("spawns.default.pitch", loc.getPitch());
-		Config.saveConfig(spawnConfigFile, spawnConfig);
-	}
-
+    private void setDefaultSpawn(Location loc) {
+        File spawnConfigFile = Config.getConfigFile("spawns.yml");
+        YamlConfiguration spawnConfig = Config.getCustomConfig(spawnConfigFile);
+        spawnConfig.set("spawns.default.world", loc.getWorld().getName());
+        spawnConfig.set("spawns.default.x", loc.getX());
+        spawnConfig.set("spawns.default.y", loc.getY());
+        spawnConfig.set("spawns.default.z", loc.getZ());
+        spawnConfig.set("spawns.default.yaw", loc.getYaw());
+        spawnConfig.set("spawns.default.pitch", loc.getPitch());
+        Config.saveConfig(spawnConfigFile, spawnConfig);
+    }
 }
