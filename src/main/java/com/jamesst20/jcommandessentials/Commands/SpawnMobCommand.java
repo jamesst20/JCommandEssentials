55a9cd7e1a8e12e86e48425bf5d0d4acd21815c0
James St-Pierre <jamesst20@gmail.com>
2012-11-24 18:10:48 -0500

    Improved Colors for SpawnmobCommand

--- src/main/java/com/jamesst20/jcommandessentials/Commands/SpawnMobCommand.java
+++ src/main/java/com/jamesst20/jcommandessentials/Commands/SpawnMobCommand.java
@@ -11,47 +11,60 @@ import com.jamesst20.jcommandessentials.Utils.Methods;
 
 public class SpawnMobCommand implements CommandExecutor {
 
-	@Override
-	public boolean onCommand(CommandSender cs, Command command, String cmd, String[] args) {
-		if (!Methods.hasPermissionTell(cs, "JCMDEss.commands.spawnmob")) {
-			return true;
-		}
-		if (Methods.isConsole(cs)) {
-			Methods.sendPlayerMessage(cs, "The console can't spawn mobs.");
-			return true;
-		}
-		if (args.length == 0) {
-			StringBuilder x = new StringBuilder();
-			for (EntityType e : EntityType.values()) {
-				x.append(e.toString()).append(", ");
-			}
-			Methods.sendPlayerMessage(cs, "You can spawn the following mobs/entities: ");
-			Methods.sendPlayerMessage(cs, x.toString().trim());
-			return true;
-		} else if (args.length == 2) {
-			int count = 0;
-			Player player = ((Player) cs);
-			try {
-				count = Integer.parseInt(args[1]);
-				for (int i = 0; i < count; i++) {
-					player.getWorld().spawnEntity(
-							player.getTargetBlock(null, 0).getLocation().getBlock().getRelative(0, 1, 0).getLocation(),
-							EntityType.valueOf(args[0].toUpperCase()));
-				}
-				Methods.sendPlayerMessage(
-						cs,
-						"You spawned " + Methods.red(String.valueOf(count)) + " "
-								+ Methods.red(EntityType.valueOf(args[0].toUpperCase()).getName()) + ".");
-				return true;
-			} catch (NumberFormatException e) {
-				Methods.sendPlayerMessage(cs, ChatColor.RED + "You must enter numbers for quantity.");
-				return true;
-			} catch (IllegalArgumentException e) {
-				Methods.sendPlayerMessage(cs, ChatColor.RED + "Invalid entity.");
-				return true;
-			}
-		} else {
-			return false;
-		}
-	}
+    @Override
+    public boolean onCommand(CommandSender cs, Command command, String cmd, String[] args) {
+        if (!Methods.hasPermissionTell(cs, "JCMDEss.commands.spawnmob")) {
+            return true;
+        }
+        if (Methods.isConsole(cs)) {
+            Methods.sendPlayerMessage(cs, "The console can't spawn mobs.");
+            return true;
+        }
+        if (args.length == 0) {
+            StringBuilder x = new StringBuilder();
+            for (EntityType e : EntityType.values()) {
+                x.append("&c").append(formatMobName(e.toString())).append("&b").append(", ");
+            }
+            Methods.sendPlayerMessage(cs, "You can spawn the following mobs/entities: ");
+            Methods.sendPlayerMessage(cs, Methods.coloring(x.toString()));
+            return true;
+        } else if (args.length == 2) {
+            int count = 0;
+            Player player = ((Player) cs);
+            try {
+                count = Integer.parseInt(args[1]);
+                for (int i = 0; i < count; i++) {
+                    player.getWorld().spawnEntity(
+                            player.getTargetBlock(null, 0).getLocation().getBlock().getRelative(0, 1, 0).getLocation(),
+                            EntityType.valueOf(args[0].toUpperCase()));
+                }
+                Methods.sendPlayerMessage(
+                        cs,
+                        "You spawned " + Methods.red(String.valueOf(count)) + " "
+                        + Methods.red(EntityType.valueOf(args[0].toUpperCase()).getName()) + ".");
+                return true;
+            } catch (NumberFormatException e) {
+                Methods.sendPlayerMessage(cs, ChatColor.RED + "You must enter numbers for quantity.");
+                return true;
+            } catch (IllegalArgumentException e) {
+                Methods.sendPlayerMessage(cs, ChatColor.RED + "Invalid entity.");
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private String formatMobName(String str) {
+        if (str.contains("_")) {
+            String[] splittedStr = str.split("_");
+            StringBuilder formattedStr = new StringBuilder();
+            for (String part : splittedStr) {
+                formattedStr.append(Character.toUpperCase(part.charAt(0))).append(part.substring(1).toLowerCase()).append("_");
+            }
+            return Methods.replaceLast(formattedStr, "_", "").toString();
+        } else {
+            return Character.toUpperCase(str.charAt(0)) + str.substring(1).toLowerCase();
+        }
+    }
 }
